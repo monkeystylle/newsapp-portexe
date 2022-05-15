a524e7583aba7a9861f79e6d01e8c66ae6a66317
meng <monkeystylle@gmail.com>
2022-05-15 08:28:21 +0800

    pagination added

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,22 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/dist/client/router';
 
 const Feed = ({ pageNumber, articles }) => {
   console.log('page:', pageNumber);
   console.log('artickes:', articles);
 
+  const router = useRouter();
+
   return (
-    <FeedWrapper>
-      {articles.map((article, index) => (
-        <ArticleWrapper key={index}>
-          <Title onClick={() => (window.location.href = article.url)}>
-            {article.title}
-          </Title>
-          <p>{article.description}</p>
-          {!!article.urlToImage && <ArticleImage src={article.urlToImage} />}
-        </ArticleWrapper>
-      ))}
-    </FeedWrapper>
+    <>
+      <FeedWrapper>
+        {articles.map((article, index) => (
+          <ArticleWrapper key={index}>
+            <Title onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </Title>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <ArticleImage src={article.urlToImage} />}
+          </ArticleWrapper>
+        ))}
+      </FeedWrapper>
+      <PaginatorWrapper>
+        <PreviousPage
+          onClick={() => {
+            if (pageNumber > 1) {
+              router
+                .push(`/feed/${pageNumber - 1}`)
+                .then(() => window.scrollTo(0, 0));
+            }
+          }}
+          pageNumber={pageNumber}
+        >
+          Previous
+        </PreviousPage>
+        <PageNumber>#{pageNumber}</PageNumber>
+        <NextPage
+          onClick={() => {
+            if (pageNumber < 5) {
+              router
+                .push(`/feed/${pageNumber - 1}`)
+                .then(() => window.scrollTo(0, 0));
+            }
+          }}
+          pageNumber={pageNumber}
+        >
+          Next
+        </NextPage>
+      </PaginatorWrapper>
+    </>
   );
 };
 
@@ -56,6 +88,9 @@ export const getServerSideProps = async pageContext => {
 
 const FeedWrapper = styled.div`
   margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const ArticleWrapper = styled.div`
   margin-top: 24px;
@@ -72,7 +107,39 @@ const Title = styled.h1`
 `;
 
 const ArticleImage = styled.img`
+  margin-top: 16px;
   width: 100%;
+`;
+
+const PaginatorWrapper = styled.div`
+  padding: 32px 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 32px;
+`;
+
+const PreviousPage = styled.div`
+  cursor: ${({ pageNumber }) => (pageNumber === 1 ? 'not-allowed' : 'pointer')};
+  color: ${({ pageNumber }) => pageNumber === 1 && '#b3b3b3'};
+`;
+
+const NextPage = styled.div`
+  cursor: ${({ pageNumber }) => (pageNumber === 5 ? 'not-allowed' : 'pointer')};
+  color: ${({ pageNumber }) => pageNumber === 5 && '#b3b3b3'};
+`;
+
+const PageNumber = styled.div`
+  cursor: pointer;
+`;
+
+const Disabled = styled.div`
+  cursor: not-allowed;
+  color: #b3b3b3;
+`;
+
+const Active = styled.div`
+  cursor: pointer;
 `;
 
 export default Feed;
